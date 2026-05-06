fd231f6d204dafffc2ad41d9c8e44aff995d964f
jkrucz <jan.kruczynski16@gmail.com>
2026-05-06 13:34:17 +0200

    Reorganized files and grouped generator settings

---
+++ Olden Era - Template Editor/Models/Generator/GeneratorSettings.cs
@@ -0,0 +1,91 @@
+namespace Olden_Era___Template_Editor.Models
+{
+    public class TournamentRules
+    {
+        public bool Enabled { get; set; } = false;
+        public int FirstTournamentDay { get; set; } = 8;
+        public int AnnouncementLeadDays { get; set; } = 3;
+        public int Interval { get; set; } = 7;
+        public int PointsToWin { get; set; } = 2;
+    }
+    public class GladiatorArenaRules
+    {
+        public bool Enabled { get; set; } = false;
+        public int DaysDelayStart { get; set; } = 30;
+        public int CountDay { get; set; } = 3;
+    }
+
+    public class GameEndConditions
+    {
+        public string VictoryCondition { get; set; } = "win_condition_1";
+        public bool LostStartCity { get; set; } = false;
+        public int LostStartCityDay { get; set; } = 3;
+        public bool LostStartHero { get; set; } = false;
+        public bool CityHold { get; set; } = false;
+        public int CityHoldDays { get; set; } = 6;
+    }
+
+    public class HeroSettings
+    {
+        public int HeroCountMin { get; set; } = 4;
+        public int HeroCountMax { get; set; } = 8;
+        public int HeroCountIncrement { get; set; } = 1;
+    }
+
+    public class AdvancedSettings
+    {
+        public bool Enabled { get; set; } = false;
+        public int NeutralLowNoCastleCount { get; set; } = 0;
+        public int NeutralLowCastleCount { get; set; } = 0;
+        public int NeutralMediumNoCastleCount { get; set; } = 0;
+        public int NeutralMediumCastleCount { get; set; } = 0;
+        public int NeutralHighNoCastleCount { get; set; } = 0;
+        public int NeutralHighCastleCount { get; set; } = 0;
+        public double PlayerZoneSize { get; set; } = 1.0;
+        public double NeutralZoneSize { get; set; } = 1.0;
+        public double GuardRandomization { get; set; } = 0.05;
+    }
+    public class ZoneConfiguration
+    {
+        public int NeutralZoneCount { get; set; } = 0;
+        public int PlayerZoneCastles { get; set; } = 1;
+        public int NeutralZoneCastles { get; set; } = 1;
+        public int ResourceDensityPercent { get; set; } = 100;
+        public int StructureDensityPercent { get; set; } = 100;
+        public int NeutralStackStrengthPercent { get; set; } = 100;
+        public int BorderGuardStrengthPercent { get; set; } = 100;
+        public AdvancedSettings Advanced { get; set; } = new AdvancedSettings();
+    }
+
+    public class GeneratorSettings
+    {
+        public string TemplateName { get; set; } = "Custom Template";
+        public string GameMode { get; set; } = "Classic";
+        public int PlayerCount { get; set; } = 2;
+        public int MapSize { get; set; } = 160;
+        public HeroSettings HeroSettings { get; set; } = new HeroSettings();
+        
+        public bool NoDirectPlayerConnections { get; set; } = false;
+        public bool RandomPortals { get; set; } = false;
+        public int MaxPortalConnections { get; set; } = 32;
+        public bool SpawnRemoteFootholds { get; set; } = true;
+        public bool GenerateRoads { get; set; } = true;
+        public bool ExperimentalBalancedZonePlacement { get; set; } = false;
+        public bool MatchPlayerCastleFactions { get; set; } = false;
+        public int MinNeutralZonesBetweenPlayers { get; set; } = 0;
+        public MapTopology Topology { get; set; } = MapTopology.Random;
+        public ZoneConfiguration ZoneCfg { get; set; } = new ZoneConfiguration();
+        public int FactionLawsExpPercent { get; set; } = 100;
+        public int AstrologyExpPercent { get; set; } = 100;
+        public GameEndConditions GameEndConditions { get; set; } = new GameEndConditions();
+        public GladiatorArenaRules GladiatorArenaRules { get; set; } = new GladiatorArenaRules();
+        public TournamentRules TournamentRules { get; set; } = new TournamentRules();
+    }
+
+    public enum NeutralZoneQuality
+    {
+        Low,
+        Medium,
+        High
+    }
+}
